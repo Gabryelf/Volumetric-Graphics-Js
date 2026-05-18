2f47d1ddeb5e67e42dcf5fa5f407d2d16020a19e
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2026-05-18 09:52:38 +0300

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,68 @@
+import * as THREE from 'three';
+import {SceneManager} from "./core/SceneManager.js";
+import {CameraManager} from "./core/CameraManager.js";
+import {LightManager} from "./core/LightManager.js";
+import {Settings} from "./utils/Settings.js"
+
+
+class Main{
+    constructor(){
+        this.sceneManager = null;
+        this.cameraManager = null;
+        this.lightManager = null;
+        this.settings = null;
+        this.renderer = null;
+        this.camera = null;
+        
+        this.time = 0;
+        this.cube = null;
+        
+        this.init()
+    }
+    
+    init(){
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.renderer.shadowMap.enable = true;
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+        document.body.appendChild(this.renderer.domElement);
+
+        this.sceneManager = new SceneManager();
+        const scene = this.sceneManager.create();
+
+        this.cameraManager = new CameraManager(this.renderer.domElement);
+        this.cameraManager.create();
+        this.cameraManager.createControls();
+        
+
+        this.lightManager = new LightManager(scene);
+        this.lightManager.createAll();
+
+        this.settings = new Settings(scene);
+        this.settings.createAllHelpers();
+        this.settings.createAllMeshes();
+        
+        window.addEventListener('resize', () => this.onWindowResize());
+
+        this.animate();
+    }
+    
+    onWindowResize(){
+        this.cameraManager.onWindowResize();
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+    
+    animate(){
+        requestAnimationFrame(() => this.animate());
+        this.time += 0.016;
+        
+        this.cameraManager.update();
+        
+        this.renderer.render(
+            this.sceneManager.getScene(),
+            this.cameraManager.getCamera()
+        )
+    }
+}
+
+const game = new Main();
