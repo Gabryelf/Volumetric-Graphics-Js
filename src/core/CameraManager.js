d488be7365ffd6f523241c7e36f9c03b61211113
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2026-05-19 09:39:55 +0300

    Update CameraManager.js

--- src/core/CameraManager.js
+++ src/core/CameraManager.js
@@ -1,9 +1,12 @@
 import * as THREE from 'three';
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js'
 import {CAMERA_CONFIG} from "../config/camera.js";
 
 export class CameraManager {
-    constructor() {
+    constructor(renderDomElement) {
         this.camera = null;
+        this.controls = null;
+        this.renderDomElement = renderDomElement;
     }
     create() {
         this.camera = new THREE.PerspectiveCamera(
@@ -24,10 +27,43 @@ export class CameraManager {
             CAMERA_CONFIG.target.y,
             CAMERA_CONFIG.target.z
         )
-        
         return this.camera
+    }
+    
+    createControls(){  //setter
+        this.controls = new OrbitControls(this.camera, this.renderDomElement);
+        this.controls.enablePan = CAMERA_CONFIG.controls.enablePan;
+        this.controls.enableDamping = CAMERA_CONFIG.controls.enableDamping;
+        this.controls.enableZoom = CAMERA_CONFIG.controls.enableZoom;
+        this.controls.dampingFactor = CAMERA_CONFIG.controls.dampingFactor;
+        this.controls.autoRotate = CAMERA_CONFIG.controls.autoRotate;
+        this.controls.rotateSpeed = CAMERA_CONFIG.controls.rotateSpeed;
+        this.controls.zoomSpeed = CAMERA_CONFIG.controls.zoomSpeed;
+        
+        this.controls.target.set(
+            CAMERA_CONFIG.target.x,
+            CAMERA_CONFIG.target.y,
+            CAMERA_CONFIG.target.z
+        );
+        
+        return this.controls;
+    }
+    
+    onWindowResize(){
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+    }
+    
+    update(){
+        if(this.controls){
+            this.controls.update();
+        }
     }
     getCamera() {
         return this.camera
+    }
+
+    getControls() { //getter
+        return this.controls;
     }
 }
