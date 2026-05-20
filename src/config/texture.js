d65c4e684f84d3f6f168e67053763fb1c2f3331c
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2026-05-20 08:22:57 +0300

    Add files via upload

---
+++ src/config/texture.js
@@ -0,0 +1,12 @@
+export const TEXTURES_CONFIG = {
+    url: {
+        ship: {
+            albedo: '../../textures/futuristic-hex-armor_albedo.png',
+            ao: '../../textures/futuristic-hex-armor_ao.png',
+            metallic: '../../textures/futuristic-hex-armor_metallic.png',
+            roughness: '../../textures/futuristic-hex-armor_roughness.png',
+            normal: '../../textures/futuristic-hex-armor_normal-ogl.png',
+            height: '../../textures/futuristic-hex-armor_height.png'
+        }
+    }
+}
---
+++ src/helpers/PartsShip.js
@@ -0,0 +1,29 @@
+import * as THREE from "three";
+import { TextureLoader } from '../core/TextureLoader.js';
+
+export class PartsShip {
+  constructor() {
+      this.ship = null;
+      this.textureLoader = new TextureLoader();
+  }
+
+  addCabin(group) {
+    this.ship = group;
+    const geometry = new THREE.CylinderGeometry(
+      1.8,
+      5,
+      5,
+      8,
+      1,
+      false,
+      0.29,
+      3,
+      );
+    const map = this.textureLoader.load(0);
+    const material = new THREE.MeshBasicMaterial({map: map});
+    material.side = 2;
+    console.log(material);
+    const cylinder = new THREE.Mesh(geometry, material);
+    this.ship.add(cylinder);
+  }
+}
