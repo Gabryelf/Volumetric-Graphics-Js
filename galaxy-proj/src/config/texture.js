fff5eda11f4637e5a9d13e2bc71039a8c80bd49e
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2026-05-20 09:45:46 +0300

    Add files via upload

---
+++ galaxy-proj/src/config/texture.js
@@ -0,0 +1,20 @@
+﻿export const TEXTURES_CONFIG = {
+    url: {
+        'hex-armor': {
+            albedo: '../../textures/hex-armor/futuristic-hex-armor_albedo.png',
+            ao: '',
+            metallic: '',
+            roughness: '',
+            normal: '',
+            height: ''
+        },
+        'rock': {
+            albedo: '../../textures/rock/wet-mossy-rocks_albedo.png',
+            ao: '../../textures/rock/wet-mossy-rocks_ao.png',
+            metallic: '../../textures/rock/wet-mossy-rocks_metallic.png',
+            roughness: '../../textures/rock/wet-mossy-rocks_roughness.png',
+            normal: '../../textures/rock/wet-mossy-rocks_normal-ogl.png',
+            height: '../../textures/rock/wet-mossy-rocks_height.png'
+        }
+    }
+}
---
+++ galaxy-proj/src/core/TextureLoader.js
@@ -0,0 +1,38 @@
+﻿import * as THREE from 'three';
+import {TEXTURES_CONFIG} from '../config/texture.js';
+
+export class TextureLoader {
+    constructor() {
+        this.texture_loader = new THREE.TextureLoader();
+    }
+    
+    load( url ) { 
+        const texture = this.texture_loader.load( 
+            url, 
+            ( texture ) => {
+                texture.colorSpace = THREE.SRGBColorSpace
+            }
+        )
+        return texture;
+    }
+    
+    load_maps(index_map) {
+        if(index_map === 1){
+            const url_albedo = TEXTURES_CONFIG.url.rock.albedo;
+            const texture = this.texture_loader.load(url_albedo);
+            return texture;
+        }
+        if(index_map === 2){
+            const url_ao = TEXTURES_CONFIG.url.rock.ao;
+            const texture = this.texture_loader.load(url_ao);
+            return texture;
+        }
+        if(index_map === 3){
+            const url_height = TEXTURES_CONFIG.url.rock.height;
+            const texture = this.texture_loader.load(url_height);
+            return texture;
+        }
+        
+    }
+    
+}
