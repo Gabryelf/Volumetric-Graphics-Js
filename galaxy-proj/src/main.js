db9e33dac917744a2538c916d665f67cbc9719de
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2026-05-27 09:52:09 +0300

    Update main.js

--- galaxy-proj/src/main.js
+++ galaxy-proj/src/main.js
@@ -1,4 +1,4 @@
-﻿import * as THREE from 'three';
+import * as THREE from 'three';
 import {SceneManager} from "./core/SceneManager.js";
 import {CameraManager} from "./core/CameraManager.js";
 import {LightManager} from "./core/LightManager.js";
@@ -6,6 +6,7 @@ import {Settings} from "./utils/Settings.js"
 import {ShipGenerator} from "./utils/ShipGenerator.js"
 import {SkySettings} from "./utils/SkySettings.js"
 import {ModelLoader} from "./core/ModelLoader.js"
+import { PaneConstructor } from './utils/PaneConstructor.js';
 
 
 class Main{
@@ -21,8 +22,14 @@ class Main{
         this.shipGenerator = null;
         this.modelLoader = null;
         
-        this.time = 0;
-        this.cube = null;
+        this.clock = null;
+        this.model = null;
+
+        this.ship = null;
+
+        this.asteroid = null;
+
+        this.paneConstructor = null;
         
         this.init()
     }
@@ -39,8 +46,8 @@ class Main{
 
         this.cameraManager = new CameraManager(this.renderer.domElement);
         this.cameraManager.create();
-        this.cameraManager.createControls();
-        
+        this.cameraManager.createFlyControls();
+        //this.cameraManager.createOrbitControls();
 
         this.lightManager = new LightManager(scene);
         this.lightManager.createAll();
@@ -53,15 +60,38 @@ class Main{
         this.skySettings.createStars();
         
         this.shipGenerator = new ShipGenerator(scene);
-        this.shipGenerator.createShip('scout');
+        //this.shipGenerator.createShip('scout');
+        //this.model = this.shipGenerator.ship
         
         this.modelLoader = new ModelLoader(scene);
-        //this.modelLoader.load(3);
+        this.modelLoader.load(0);
+
+        setTimeout(()=> {
+            this.ship = this.modelLoader.model
+            this.modelLoader.model = null;
+            this.ship.position.x = -5;
+        }, 500)
+
+        setTimeout(()=> {
+            this.modelLoader.load(3);
+        }, 1000)     
+
+        setTimeout(()=> {
+            this.asteroid = this.modelLoader.model
+            this.asteroid.position.x = 5;
+        }, 4000)   
+
+        this.clock = new THREE.Clock();
+
+        this.paneConstructor = new PaneConstructor(scene);
+
+        //setTimeout(() => {this.model = this.modelLoader.model; this.model.name = 'Model';this.paneConstructor.addAllPanels(this.model);}, 500);
         
         window.addEventListener('resize', () => this.onWindowResize());
 
         this.animate();
     }
+
     
     onWindowResize(){
         this.cameraManager.onWindowResize();
@@ -70,13 +100,30 @@ class Main{
     
     animate(){
         requestAnimationFrame(() => this.animate());
-        this.time += 0.016;
+
+        const delta = this.clock.getDelta();
         
-        this.cameraManager.update();
-        
+        this.cameraManager.update(delta);
+
+        if(this.asteroid){
+            //this.asteroid.position.x -= 0.01;
+        }
+
+        if(this.ship){
+            //this.ship.position.x += 0.01;
+        }
+
+        if(this.asteroid && this.ship){  // есть ли астероид и корабль, если есть проверяем дистанцию
+            if(this.ship.position.distanceTo(this.asteroid.position) < 1){ 
+                this.sceneManager.scene.remove(this.asteroid);
+                this.asteroid = null;
+            }
+        }
+
         this.renderer.render(
             this.sceneManager.getScene(),
-            this.cameraManager.getCamera()
+            this.cameraManager.getCamera(),
+            this.camera = this.cameraManager.getCamera()
         )
     }
 }
