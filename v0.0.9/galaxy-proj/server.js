92657c52a781c3b50c878b2797d8a94929ff9a98
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2026-06-06 21:23:27 +0300

    update_version_0.0.9
    fix links & add socket to server

--- v0.0.9/galaxy-proj/server.js
+++ v0.0.9/galaxy-proj/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import {createServer} from 'http';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 import { fileURLToPath } from 'url';
-import {dirname, join} from 'path';
+import { dirname, join } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,10 +11,26 @@ const app = express();
 const server = createServer(app);
 const PORT = process.env.PORT || 3000;
 
+const io = new Server(server, {cors:{origin: '*'}});
+const players = new Map();
+
+// Раздаём статические файлы из корня
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+    console.log(`------ Игрок подключился: ${socket.id} -------`);
+
+    socket.emit('your-id', socket.id);
+
+    socket.on('disconect', () => {
+        console.log(`====== Игрок отключился: ${socket.id} =======`);
+        players.delete(socket.id);
+        socket.broadcast.emit('player-disconected', socket.id);
+    });
 });
 
 server.listen(PORT, () => {
