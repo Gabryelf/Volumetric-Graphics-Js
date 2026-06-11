7e97a4b14f3cf45011c94367c4bc3928dd83fb99
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2026-06-11 08:13:56 +0300

    Delete galaxy-proj/src directory

--- galaxy-proj/src/config/model.js
+++
@@ -1,8 +0,0 @@
-﻿export const MODELS_CONFIG = {
-    url : [
-        '../../models/assault.glb',
-        '../../models/scout.glb',
-        '../../models/asteroid.glb',
-        '../../models/asteroid2.glb'
-    ]
-}
